9a4ce48e94f450149e0732e2ddf4f5b542089cf7
AusSS7 <riolesteban05@gmail.com>
2023-10-22 18:34:25 -0700

    6. Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str.replace("language that combines remarkable power with very clear syntax", "with python")
-str = str.split(',')
-str = str[2:7]
+str1,str2,str3 = str.split(',')
+str = str3.replace("language that combines remarkable power with very clear syntax", "with python")
 print(str)
